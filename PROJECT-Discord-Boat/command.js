9b12fd9dae2b7e6b1c2cdf5aab37c704aa9bd856
ambarGupta <ambar.gtm@gmail.com>
2025-11-01 23:43:24 +0530

    Add command handling for 'ping' and 'create' in index.js and implement command registration in command.js

---
+++ PROJECT-Discord-Boat/command.js
@@ -0,0 +1,27 @@
+const  { REST, Routes }  = require("discord.js")
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+   {
+    name: 'create',
+    description: 'create a short url for any given Url',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken("MTQzMzUxMTEwNDM1NjgxMDgxMg.G-wNM6.1fgK4U_uY4yDGfQ_qzhjBzXxKPZU0H5H32KTSw");
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands("1433511104356810812"), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+
--- PROJECT-Discord-Boat/index.js
+++ PROJECT-Discord-Boat/index.js
@@ -9,6 +9,23 @@ client.on("messageCreate",(message) => {
     message.reply({
         content:"Hi from Bot",
     });
+
+    if(message.content.startsWith("create"))
+    {
+         const url = message.content.split("create")[1];
+          console.log("url",url)
+          message.reply("shorturl for your Url is : "+ url);
+    }
 });
 
-client.login("");
+client.on("interactionCreate",(interaction) => {
+    if(!interaction.isChatInputCommand())
+           return ;
+    if(interaction.commandName === 'ping')
+         interaction.reply("Pong!");
+ 
+         
+});
+
+
+client.login("MTQzMzUxMTEwNDM1NjgxMDgxMg.G-wNM6.1fgK4U_uY4yDGfQ_qzhjBzXxKPZU0H5H32KTSw");
